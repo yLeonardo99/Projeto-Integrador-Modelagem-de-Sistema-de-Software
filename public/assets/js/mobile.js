90662e41b6776fee653c86ec9761cbb8fafd932c
yLeonardo99 <leo205283@gmail.com>
2025-04-12 23:27:41 -0300

    Leonardo C.
    Ajustes feitos;
    
    Front-End;
    
    1. JAVASCRIPT
    
    • Feito o arquivo 'orçamentoCliente.js' objetivo fazer que verifica se o usuário fez uma sessão ou seja tenha feito o login, caso tenha feito o usuário é redirecionado a pagina "orçamentoCliente.html" podendo preencher o formulário, caso o usuário não tenha feito o login exibirá um SPAN dizendo para que o usuário tem que fazer login ou cadastrar-se para preenchimento.
    
    • Feito script para função MOBILE.
    
    ** SASS / CSS ***
    
    - Implementei menu hamburguer para dispositivos móveis.
    - Ajustando os  layout para coluna única em telas menores
    - Mantém estilos desktop existentes
    - Compilei SASS para CSS otimizado"
    - nome do arquivo "mobile.css"

---
+++ public/assets/js/mobile.js
@@ -0,0 +1,19 @@
+// Menu Hamburguer
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('header nav');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+});
+
+// Fechar menu ao clicar em qualquer link
+
+const navLinks = document.querySelectorAll('nav ul li a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuToggle.classList.remove('active');
+        nav.classList.remove('active');
+    });
+});
